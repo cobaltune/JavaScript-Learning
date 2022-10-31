9535ee5419da4079edcece5305b6ede16280c7b8
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-11-01 01:26:59 +0900

    edit

--- SQL test result_for...in.js
+++ SQL test result_for...in.js
@@ -26,9 +26,19 @@ let minsoonScore = {
 	'SQL 최적화 기본 원리': 16,
 };
 
-function passChecker(scoreObject) {
-	// 여기에 코드를 작성해 주세요.
+function passChecker (scoreObject) {
 
+let testResult = 0;
+  
+  for (let key in scoreObject) {
+    testResult += scoreObject [key];
+  }
+    
+  if (testResult >= 60) {
+  	console.log (`축하합니다! 합격입니다!`);
+    } else {
+  	console.log (`아쉽지만 불합격입니다..`);
+  }
 }
 
 passChecker(hyesoonScore);
