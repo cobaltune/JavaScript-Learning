b0faaa4397f737bf7d7062d2498c6cdefaa30676
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-11-05 21:18:39 +0900

    edit

--- date.js
+++ date.js
@@ -8,6 +8,7 @@
 만약 8월 24일에 입사했고,
 오늘이 8월 24일이면 0일이 아니라 '입사한 지 1일째'입니다. */
 
+
 // my answer
 let today = new Date(2112, 8, 24);
 let jaeSangStart = new Date(2109, 7, 1);
@@ -29,7 +30,7 @@ let jaeSangStart = new Date(2109, 7, 1);
 
 let dday = today - jaeSangStart;
 function workDayCalc(startDate) {
-	// 몇일째 일하는 중인지 출력
+	// 며칠째 일하는 중인지 출력
 	console.log(`오늘은 입사한 지 ${dday / 1000 / 60 / 60 / 24 + 1}일째 되는 날 입니다.`);
 }
 
