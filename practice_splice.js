f048a76eab780e49bce881866d781e48457f56c1
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-11-09 21:32:21 +0900

    edit

--- practice_splice.js
+++ practice_splice.js
@@ -14,16 +14,21 @@ let fruits = ['레몬', '토마토', '딸기', '바나나'];
 let ages = [20, 24, 25, 29, 30, 33];
 let numbers = [];
 
+// fruits 배열에 '토마토'를 삭제하고 그 자리에 '사과', '청포도' 를 추가해 주세요.
 fruits.splice (1, 1, '사과', '청포도');
 
+// fruits 배열의 첫 번째 요소를 삭제해 주세요.
 fruits.splice (0, 1);
 
+// ages 배열에 마지막 요소를 삭제해 주세요.
 ages.splice (ages.length - 1);
 
+// ages 배열의 2번, 3번 인덱스를 26, 28로 변경해 주세요.
 ages[2] = '26';
 ages[3] = '28';
 
-numbers = 1, 2, 3, 5, 8, 9;
+// numbers 배열에 1, 2, 3, 5, 8, 9를 순서대로 추가해 주세요.
+numbers.splice(0, 0, 1, 2, 3, 5, 8, 9);
 
 // 반복문을 활용해서 numbers 배열의 요소들 중 홀수를 모두 삭제해 주세요.
 for (let i = 0; i < numbers.length; i++) {
